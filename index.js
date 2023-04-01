bf4e336f42c5821f01302d1fe941da0b136a900c
Joseph Sam <joseph.sam@amalitech.com>
2023-04-01 15:19:18 +0000

    Saving data to MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import data from "./data.js";
 import cors from "cors";
+import { User as DBModel } from "./module/phonebook.js";
 
 // const requestLogger = (request, response, next) => {
 //    console.log('Method:', request.method)
@@ -11,18 +12,35 @@ import cors from "cors";
 //    next();
 //  }
 
+const errorHandler = (error, request, response, next) => {
+   console.error(error.message);
+   console.log('Error: ', error.name)
+      if(error.name === 'CastError') return response.status(400).send({ error: 'malFormatted id' });
+      else if(error.name === 'ValidationError') return response.status(400).json({ error: error.message })
+
+   next(error);
+};
+const unknownEndpoint = (request, response) => {
+   response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
-
+app.use(express.static('build'));
 app.use(express.json());
+app.use(cors());
 morgan.token('data',(req, res) => { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-app.use(cors());
-app.use(express.static('build'));
 
-app.get('/api/persons', (req, res) => {
-   res.json(data);
+app.get('/api/persons', async (req, res) => {
+   try {
+      let result = await DBModel.find({});
+      res.json(result);
+   } catch (error) {
+      console.log(error.message)
+   }
 });
 
 app.get('/info', (req, res) => {
@@ -37,56 +55,93 @@ app.get('/info', (req, res) => {
    );
 });
 
-app.get('/api/persons/:id', (req, res) => {
-   const id = Number(req.params.id);
-   const personInfo = data.find(person => person.id === id);
-   
-   personInfo ? res.json(personInfo) : res.status(404).end()
-});
-
-app.put('/api/persons/:id', (req, res) => {
-   const content = req.body;
-   // console.log(content);
-   if (!content) {
-      return res.status(404)
-         .json({ error: 'Content missing'});
-   };
-
-   const id = Number(req.params.id);
-   const personInfo = data.filter(person => person.id !== id);
-   const oldPerson = data.find(person => {
-
-      // console.log('person', person.id)
-      return person.id === id
-   });
-
-   // console.log('OldPerson: ', oldPerson)
-   // console.log('Id: ',id)
-
-   res.json(personInfo.concat({...oldPerson, ...content}));
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-   const id = Number(req.params.id);
-   const personInfo = data.filter(person => person.id !== id);
-   personInfo.length ? res.status(204).end() : res.status(404).end()
-});
-
-app.post('/api/persons', (req, res) => {
-   const content = req.body;
-   // console.log(content);
-   if (!content) {
-      return res.status(404)
-         .json({ error: 'Content missing'});
+app.get('/api/persons/:id', async (req, res, next) => {
+   const id = req.params.id;
+   // const personInfo = data.find(person => person.id === id);
+   // personInfo ? res.json(personInfo) : res.status(404).end();
+   try {
+      let result = await DBModel.findById(id);
+      if(result) {
+         res.json(result);
+      } else {
+         res.status(404).end();
+      }
+   } catch (error) {
+      next(error);
    }
+});
 
-   const newPerson = {
-      id: Math.floor(1 + Math.random()*20),
+app.put('/api/persons/:id', async (req, res, next) => {
+   const content = req.body;
+   // if (!content) {
+   //    return res.status(404)
+   //       .json({ error: 'Content missing'});
+   // };
+
+   const id = req.params.id;
+   try {
+      await DBModel.findByIdAndUpdate(id, content, {
+         new: true,
+         runValidators: true,
+         context: 'query'
+      });
+
+      let result = await DBModel.find({});
+      res.json(result);
+   } catch (error) {
+      return next(error);
+   }
+   // const personInfo = data.filter(person => person.id !== id);
+   // const oldPerson = data.find(person => {
+      
+      //    // console.log('person', person.id)
+   //    return person.id === id
+   // });
+   
+   // // console.log('OldPerson: ', oldPerson)
+   // // console.log('Id: ',id)
+   
+   // res.json(personInfo.concat({...oldPerson, ...content}));
+});
+
+app.delete('/api/persons/:id', async (req, res, next) => {
+   const id = req.params.id;
+   try {
+      const results = await DBModel.findByIdAndRemove(id);
+      res.json(results);
+   } catch (error) {
+      console.log(error.message);
+   }
+   // const personInfo = data.filter(person => person.id !== id);
+   // personInfo.length ? res.status(204).end() : res.status(404).end()
+});
+
+app.post('/api/persons', async (req, res, next) => {
+   const content = req.body;
+   
+   // if (!content) {
+   //    return res.status(404)
+   //    .json({ error: 'Content missing'});
+   // };
+   
+   const persons = new DBModel({
       name: content?.name,
       number: content?.number,
+   });
+
+   try {
+      const newPerson = await persons.save();
+      console.log(newPerson);
+      let result = await DBModel.find({});
+      res.json(result);
+      
+   } catch (error) {
+      next(error);
    }
-   res.json(data.concat(newPerson));
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.listen(port);
 console.log(`listening on ${port}`);
---
+++ module/phonebook.js
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+const phoneBookSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      minLength: 3,
+      require: [true, 'User name required'],
+   },
+   number: {
+      type: String,
+      validate: {
+         validator: function(v) {
+           return /\d{3}-\d{3}-\d{4}/.test(v);
+         },
+         message: props => `${props.value} is not a valid phone number!`
+       },
+       required: [true, 'User phone number required']
+     }
+});
+
+const User = mongoose.model('phonebook', phoneBookSchema);
+
+phoneBookSchema.set('toJSON', {
+   transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString();
+      delete returnedObject._id;
+      delete returnedObject.__v;
+   }
+});
+
+const connectDB = async () => {
+   try {
+      const res = await mongoose.connect(url);
+      console.log('connected to MongoDB')
+      return User;
+   } catch (error) {
+      console.log('error connecting to MongoDB: ', error.message);
+   }
+}
+connectDB();
+
+export { User };
