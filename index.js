113c8475eb12b06b5585bc0ff6a390a74bfa0838
Joseph Sam <joseph.sam@amalitech.com>
2023-04-01 17:46:21 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import data from "./data.js";
-import cors from "cors";
-import { User as DBModel } from "./module/phonebook.js";
+/* eslint-disable no-undef */
+import express from 'express';
+import morgan from 'morgan';
+import data from './data.js';
+import cors from 'cors';
+import { User as DBModel } from './module/phonebook.js';
 
 // const requestLogger = (request, response, next) => {
 //    console.log('Method:', request.method)
@@ -13,16 +14,16 @@ import { User as DBModel } from "./module/phonebook.js";
 //  }
 
 const errorHandler = (error, request, response, next) => {
-   console.error(error.message);
-   console.log('Error: ', error.name)
-      if(error.name === 'CastError') return response.status(400).send({ error: 'malFormatted id' });
-      else if(error.name === 'ValidationError') return response.status(400).json({ error: error.message })
+  console.error(error.message);
+  console.log('Error: ', error.name);
+  if(error.name === 'CastError') return response.status(400).send({ error: 'malFormatted id' });
+  else if(error.name === 'ValidationError') return response.status(400).json({ error: error.message });
 
-   next(error);
+  next(error);
 };
 const unknownEndpoint = (request, response) => {
-   response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 
 const app = express();
@@ -31,113 +32,113 @@ const port = process.env.PORT;
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
-morgan.token('data',(req, res) => { return JSON.stringify(req.body)})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+morgan.token('data',(req) => { return JSON.stringify(req.body);});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 app.get('/api/persons', async (req, res) => {
-   try {
-      let result = await DBModel.find({});
-      res.json(result);
-   } catch (error) {
-      console.log(error.message)
-   }
+  try {
+    let result = await DBModel.find({});
+    res.json(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.get('/info', (req, res) => {
   const info = {
-   entries: data.length,
-   date: new Date().toString()
-  }
-   res.send(
-      `<p>Phonebook has info for ${info.entries} people</p>
+    entries: data.length,
+    date: new Date().toString()
+  };
+  res.send(
+    `<p>Phonebook has info for ${info.entries} people</p>
       <p>${info.date}</p>
       `
-   );
+  );
 });
 
 app.get('/api/persons/:id', async (req, res, next) => {
-   const id = req.params.id;
-   // const personInfo = data.find(person => person.id === id);
-   // personInfo ? res.json(personInfo) : res.status(404).end();
-   try {
-      let result = await DBModel.findById(id);
-      if(result) {
-         res.json(result);
-      } else {
-         res.status(404).end();
-      }
-   } catch (error) {
-      next(error);
-   }
+  const id = req.params.id;
+  // const personInfo = data.find(person => person.id === id);
+  // personInfo ? res.json(personInfo) : res.status(404).end();
+  try {
+    let result = await DBModel.findById(id);
+    if(result) {
+      res.json(result);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.put('/api/persons/:id', async (req, res, next) => {
-   const content = req.body;
-   // if (!content) {
-   //    return res.status(404)
-   //       .json({ error: 'Content missing'});
-   // };
+  const content = req.body;
+  // if (!content) {
+  //    return res.status(404)
+  //       .json({ error: 'Content missing'});
+  // };
 
-   const id = req.params.id;
-   try {
-      await DBModel.findByIdAndUpdate(id, content, {
-         new: true,
-         runValidators: true,
-         context: 'query'
-      });
+  const id = req.params.id;
+  try {
+    await DBModel.findByIdAndUpdate(id, content, {
+      new: true,
+      runValidators: true,
+      context: 'query'
+    });
 
-      let result = await DBModel.find({});
-      res.json(result);
-   } catch (error) {
-      return next(error);
-   }
-   // const personInfo = data.filter(person => person.id !== id);
-   // const oldPerson = data.find(person => {
-      
-      //    // console.log('person', person.id)
-   //    return person.id === id
-   // });
-   
-   // // console.log('OldPerson: ', oldPerson)
-   // // console.log('Id: ',id)
-   
-   // res.json(personInfo.concat({...oldPerson, ...content}));
+    let result = await DBModel.find({});
+    res.json(result);
+  } catch (error) {
+    return next(error);
+  }
+  // const personInfo = data.filter(person => person.id !== id);
+  // const oldPerson = data.find(person => {
+
+  //    // console.log('person', person.id)
+  //    return person.id === id
+  // });
+
+  // // console.log('OldPerson: ', oldPerson)
+  // // console.log('Id: ',id)
+
+  // res.json(personInfo.concat({...oldPerson, ...content}));
 });
 
-app.delete('/api/persons/:id', async (req, res, next) => {
-   const id = req.params.id;
-   try {
-      const results = await DBModel.findByIdAndRemove(id);
-      res.json(results);
-   } catch (error) {
-      console.log(error.message);
-   }
-   // const personInfo = data.filter(person => person.id !== id);
-   // personInfo.length ? res.status(204).end() : res.status(404).end()
+app.delete('/api/persons/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const results = await DBModel.findByIdAndRemove(id);
+    res.json(results);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // const personInfo = data.filter(person => person.id !== id);
+  // personInfo.length ? res.status(204).end() : res.status(404).end()
 });
 
 app.post('/api/persons', async (req, res, next) => {
-   const content = req.body;
-   
-   // if (!content) {
-   //    return res.status(404)
-   //    .json({ error: 'Content missing'});
-   // };
-   
-   const persons = new DBModel({
-      name: content?.name,
-      number: content?.number,
-   });
+  const content = req.body;
 
-   try {
-      const newPerson = await persons.save();
-      console.log(newPerson);
-      let result = await DBModel.find({});
-      res.json(result);
-      
-   } catch (error) {
-      next(error);
-   }
+  // if (!content) {
+  //    return res.status(404)
+  //    .json({ error: 'Content missing'});
+  // };
+
+  const persons = new DBModel({
+    name: content?.name,
+    number: content?.number,
+  });
+
+  try {
+    const newPerson = await persons.save();
+    console.log(newPerson);
+    let result = await DBModel.find({});
+    res.json(result);
+
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use(unknownEndpoint);
--- mongo.js
+++ mongo.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const argumentLength = process.argv.length;
 console.log(argumentLength);
 if (argumentLength < 3) {
-   console.log('give password as argument');
-   process.exit(1);
- }
+  console.log('give password as argument');
+  process.exit(1);
+}
 
- if(argumentLength > 5) {
-   console.log('enclosed name in quotes');
-   process.exit(1);
- }
- const password = process.argv[2];
- const name = process.argv[3];
- const phoneNumber = process.argv[4];
+if(argumentLength > 5) {
+  console.log('enclosed name in quotes');
+  process.exit(1);
+}
+const password = process.argv[2];
+const name = process.argv[3];
+const phoneNumber = process.argv[4];
 
 const url = `mongodb+srv://fullStackOpen:${password}@cluster0.q4prcr2.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
@@ -21,26 +21,26 @@ mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const phoneBookSchema = new mongoose.Schema({
-   name: String,
-   phoneNumber: String,
+  name: String,
+  phoneNumber: String,
 });
 const User = mongoose.model('phonebook', phoneBookSchema);
 
 if(name && phoneNumber) {
-   const details = new User({
-      name,
-      phoneNumber,
-   });
-   
-   details.save().then((results) => {
-      const { name, phoneNumber } = results;
-      console.log(`added ${name} number ${phoneNumber} to ${results.db.name}`);
-      mongoose.connection.close();
-   });
+  const details = new User({
+    name,
+    phoneNumber,
+  });
+
+  details.save().then((results) => {
+    const { name, phoneNumber } = results;
+    console.log(`added ${name} number ${phoneNumber} to ${results.db.name}`);
+    mongoose.connection.close();
+  });
 } else {
-   User.find({}).then(results => {
-      console.log('phonebook:')
-      results.forEach(contacts => console.log(`${contacts.name} ${contacts.phoneNumber}`));
-      mongoose.connection.close();
-   });
+  User.find({}).then(results => {
+    console.log('phonebook:');
+    results.forEach(contacts => console.log(`${contacts.name} ${contacts.phoneNumber}`));
+    mongoose.connection.close();
+  });
 }
